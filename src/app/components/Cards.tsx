8deec47edb9f0077372016acaf7ea0345298582d
Royweru <royezi17@gmail.com>
2023-08-14 19:11:30 +0300

    new file:   public/download1.jpeg 	modified:   src/app/components/Cards.tsx 	modified:   src/app/page.tsx

--- src/app/components/Cards.tsx
+++ src/app/components/Cards.tsx
@@ -1,22 +1,35 @@
-import React from 'react'
-import fetchAllPosts from '../../../lib/allposts'
-const  Cards = async() => {
-    const allposts:Promise<[]> = fetchAllPosts()
-    const blogs:post[] = await allposts
+import React from "react";
+import fetchAllPosts from "../../../lib/allposts";
+const Cards = async () => {
+  const allposts: Promise<[]> = fetchAllPosts();
+  const blogs: post[] = await allposts;
 
   return (
-   <div>
-    {blogs.map(blog=>(
-       <>
-        <h1>
-            {blog.title}
-        </h1>
-        
-       </>
-    ))}
-   </div>
-    
-  )
-}
+    <section className="w-full ">
+      {blogs.map((blog) => (
+        <>
+          <div className="m-auto overflow-hidden rounded-lg shadow-lg cursor-pointer h-90 w-60 md:w-80">
+            <a href="#" className="block w-full h-full">
+              <img
+                alt="blog photo"
+                src=""
+                class="object-cover w-full max-h-40"
+              />
+              <div className="w-full p-4 bg-white dark:bg-gray-800">
+                <p className="font-medium text-indigo-500 text-md"></p>
+                <p className="mb-2 text-xl font-medium text-gray-800 dark:text-white">
+                 {blog.title}
+                </p>
+                <p className="font-light text-gray-400 dark:text-gray-300 text-md">
+                  {blog.body.slice(0,50)}...
+                </p>
+              </div>
+            </a>
+          </div>
+        </>
+      ))}
+    </section>
+  );
+};
 
-export default Cards
+export default Cards;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,9 @@ import Cards from "./components/Cards"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <div>
+    < >
       <Cards />
-     </div>
-    </main>
+     
+    </>
   )
 }
